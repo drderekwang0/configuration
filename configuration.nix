1f8cc42eb7f300306406588e789d5b5039e9adc3
Derek Wang <dr.derekwang0@gmail.com>
2023-05-10 21:21:27 +0000

    Enable ZFS

--- configuration.nix
+++ configuration.nix
@@ -113,5 +113,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
+  # Enable ZFS.
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  networking.hostId = "7229408b";
 }
 
