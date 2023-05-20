73684d94d86b622173168fd650ed87dba3d85e3b
Derek Wang <dr.derekwang0@gmail.com>
2023-05-20 15:39:47 +0000

    Enable SSH

--- configuration.nix
+++ configuration.nix
@@ -94,6 +94,9 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  # Enable SSH.
+  services.openssh.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
